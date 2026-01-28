60e5ad0777992e2c2ca6a2bb3cb112d0b408e15d
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2026-01-28 22:11:26 +0700

    update
    call out maydays

--- ocx-ticketing/src/app/components/ocx5/OCX5LineupSection.tsx
+++ ocx-ticketing/src/app/components/ocx5/OCX5LineupSection.tsx
@@ -31,7 +31,7 @@ export default function OCX5LineupSection({ debugLayout }: { debugLayout: boolea
       },
       {
         id: 4,
-        image: "/images/ocx5_images/lineup/OCX5_lineup_main_artist1_blind_alt1.png",
+        image: "/images/ocx5_images/lineup/OCX5_lineup_main_artist1_maydays_alt1.png",
       },
       {
         id: 5,
